f08b6446000eb7daf6560537d3e5688dd529bccf
a105590016 <love11662255@gmail.com>
2023-10-31 18:29:22 +0800

    feat: 🎸 admin 過濾器, admin insert 區塊化, admin 列表顯示欄位

--- online/admin.py
+++ online/admin.py
@@ -8,6 +8,17 @@ from online.models import Employer, Store, Product
 
 class EmployerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'age')
+    list_filter = ('first_name', 'age')
+    # 用 () 包起來之後, 會變成並列顯示
+    # fields = [('first_name', 'last_name'), 'age']
+    fieldsets = (
+        ('name', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('info', {
+            'fields': ('age', 'other field')
+        }),
+    )
     
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
