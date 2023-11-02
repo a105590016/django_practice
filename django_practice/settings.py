efed68dec6b0ab3bd6862e699275c59513320014
a105590016 <love11662255@gmail.com>
2023-11-02 12:28:14 +0800

    feat: 🎸 登入登出, 重設密碼

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'django_practice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': ['./django_practice/templates', ],
-        'DIRS': [],
+        'DIRS': ['./django_practice/templates', ],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/online/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
